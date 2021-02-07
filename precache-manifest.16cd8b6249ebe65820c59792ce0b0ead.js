8444337b0ccb466ba58011e7768249820aec95bc
mouveee <mouvy@web.de>
2021-02-07 20:58:28 +0100

    new borders

---
+++ precache-manifest.16cd8b6249ebe65820c59792ce0b0ead.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57014ed58db4bf9e1858033acc1f10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bca4b98f94659b169a3",
+    "url": "/static/css/main.f2c3bdf9.chunk.css"
+  },
+  {
+    "revision": "42aa61814c702d5fa76e",
+    "url": "/static/js/2.ae36cc4c.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.ae36cc4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bca4b98f94659b169a3",
+    "url": "/static/js/main.cb5f1bd9.chunk.js"
+  },
+  {
+    "revision": "31c0e678ab4c5d34cd2c",
+    "url": "/static/js/runtime-main.42483bc1.js"
+  },
+  {
+    "revision": "77c49338f87054c630655d41b46952f0",
+    "url": "/static/media/MS_EXAM.77c49338.png"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "02e9ea86a845de7bf3b29052529efc11",
+    "url": "/static/media/head.02e9ea86.mp4"
+  }
+]);
