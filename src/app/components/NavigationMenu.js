6436e42a5dca81b74866dcf804716824d963029e
mouveee <huwig.marco@gmail.com>
2025-03-14 11:12:11 +0100

    NavigationMenu: Remove duplicate import

--- src/app/components/NavigationMenu.js
+++ src/app/components/NavigationMenu.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavigationMenu = () => {
   const [isHovered, setIsHovered] = useState(false);
