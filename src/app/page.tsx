11baa03923c7ad98706c479589d69f7d93b08ff3
mouveee <huwig.marco@gmail.com>
2025-04-13 21:03:55 +0200

    HomeScreen: Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,12 @@ import Link from 'next/link';
 import Image from 'next/image';
 import arrowDown from '../assets/arrow_down.jpg';
 
-// Constants for video grid
-const INNER_WIDTH = 1280;
-const INNER_HEIGHT = 720;
-const ROWS = 4; 
-const COLS = 7;
+let INNER_WIDTH = 0;
+let INNER_HEIGHT = 0;
+const ROWS = 3;
+const COLS = 6;
 const NUM_PIECES = ROWS * COLS;
 
-// Types
 interface CanvasStyle {
   width: number;
   height: number;
@@ -41,6 +39,7 @@ export default function Home() {
   const [arrowVisible, setArrowVisible] = useState<boolean>(false);
   const [fadeOut, setFadeOut] = useState<boolean>(false);
   const [canvasStyles, setCanvasStyles] = useState<CanvasStyle[]>([]);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const homescreenRef = useRef<HTMLDivElement | null>(null);
   const videoRef = useRef<HTMLVideoElement | null>(null);
@@ -48,12 +47,18 @@ export default function Home() {
   const linkRef = useRef<HTMLAnchorElement | null>(null);
 
   useEffect(() => {
-    const handleScroll = async (event: WheelEvent) => {
-      if (event.deltaY > 0 && homescreenRef.current) {            
-        homescreenRef.current.style.transition = "transform 5s ease-in-out";
-        homescreenRef.current.style.transform = "translateY(3000vh)";
+    if (homescreenRef.current) {
+      INNER_WIDTH = window.innerWidth * 11 / 12;
+      INNER_HEIGHT = window.innerHeight * 11 / 12;
+    }
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
 
-          linkRef.current?.click();
+    const handleScroll = async (event: WheelEvent) => {
+      if (event.deltaY > 0 && homescreenRef.current) {
+        linkRef.current?.click();
         window.removeEventListener("wheel", handleScroll);
       }
     };
@@ -69,9 +74,9 @@ export default function Home() {
       return {
         width: baseWidth,
         height: baseHeight,
-        margin: 5,
+        margin: 2,
         padding: 1,
-        opacity: 0.3 + Math.random() * 0.9,
+        opacity: 0.7 + Math.random() * 0.1,
         left: col * baseWidth,
         top: row * baseHeight,
         randomOffset: {
@@ -109,30 +114,33 @@ export default function Home() {
     function draw() {
       const video = videoRef.current;
       if (!video || video.paused || video.ended) return;
-
+    
       canvasRefs.current.forEach((canvas, index) => {
         if (!canvas) return;
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext("2d");
         if (!ctx) return;
-
+    
         const style = canvasStyles[index];
         if (!style) return;
-
+    
         const row = Math.floor(index / COLS);
         const col = index % COLS;
-        const sourceWidth = INNER_WIDTH / COLS;
-        const sourceHeight = INNER_HEIGHT / ROWS;
-
-        ctx.clearRect(0, 0, style.width + style.randomOffset.width, style.height + style.randomOffset.height);
+    
+        const sourceWidth = (video.videoWidth / COLS);
+        const sourceHeight = (video.videoHeight / ROWS);
+    
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
         ctx.drawImage(
           video,
           col * sourceWidth, row * sourceHeight, sourceWidth, sourceHeight,
-          0, 0, style.width + style.randomOffset.width, style.height + style.randomOffset.height
+          0, 0, canvas.width, canvas.height
         );
       });
-
+    
       requestAnimationFrame(draw);
     }
+    
 
     const handleVideoPlay = () => {
       draw();
@@ -143,8 +151,9 @@ export default function Home() {
       if (!video) return;
 
       video.addEventListener('play', handleVideoPlay);
-      video.addEventListener('playing', handleVideoPlay);}
-    
+      video.addEventListener('playing', handleVideoPlay);
+    }
+
 
     return () => {
       if (video) {
@@ -178,22 +187,17 @@ export default function Home() {
       width: `${style.width + style.randomOffset.width}px`,
       height: `${style.height + style.randomOffset.height}px`,
       mixBlendMode: "screen" as const,
-      borderRadius: "50px",
-      filter: "blur(0px) brightness(0.8) contrast(1.2) grayscale(0.5) hue-rotate(0deg) saturate(1.5)",
+      borderRadius: "12px",
+      filter: "blur(1px) contrast(1.1) grayscale(0.8) hue-rotate(0deg) saturate(0.1)",
     };
   };
 
   return (
     <div
-      className={`relative homescreen min-h-screen max-h-screen h-[100vh] overflow-hidden flex items-center justify-center bg-black ${fadeOut ? "opacity-0 transition-opacity duration-[3000s]" : ""}`}
+      className={`relative m-auto homescreen min-h-screen h-[100vh] overflow-y-clip flex items-center justify-center bg-black ${fadeOut ? "opacity-0 transition-opacity duration-[3000s]" : ""} bg-gradient-to-r from-black via-[#4c09325c] to-black animate-bgMove`}
       ref={homescreenRef}>
       <div
-        className="absolute overflow-hidden w-full h-full"
-        style={{
-          top: "0",
-          left: "0",
-          // transform: "translate(-50%, -50%)",
-        }}
+        className="relative m-auto overflow-y-clip w-11/12 h-[91.6%] items-center justify-center"
       >
         {/* Loading Spinner */}
         {isLoading && (
@@ -207,14 +211,14 @@ export default function Home() {
           <div className="absolute" key={index}>
             <canvas
               ref={(el) => { canvasRefs.current[index] = el; }}
-              className="absolute flicker inset-0 transform rotateY-180"
+              className="absolute flicker inset-0 grayscale"
               style={getCanvasStyle(style)}
               width={style.width + style.randomOffset.width}
               height={style.height + style.randomOffset.height}
             ></canvas>
 
             <div className="absolute inset-0 bg-pink-500 pointer-events-none flicker"
-              style={{ ...getCanvasStyle(style), opacity: Math.random() * 0.1, zIndex: 10, filter: "none" }}
+              style={{ ...getCanvasStyle(style),opacity: 0.02 +  Math.random() * 0.1, zIndex: 10, filter: "none" }}
             >
             </div>
           </div>
@@ -224,11 +228,15 @@ export default function Home() {
 
       <Link
         href="/intro"
-        className={`absolute w-5 h-5 border-r-yellow-950 bottom-12 right-10 transition-opacity duration-1000 z-50 ease-in-out ${arrowVisible ? "opacity-100" : "opacity-0"}`}
+        className={isMobile ? 
+          "absolute w-5 h-5 border-r-yellow-950 bottom-12 right-10 transition-opacity duration-1000 z-50 ease-in-out rounded-full bg-pink-500 shadow-lg cursor-pointer animate-bounce z-49 border-2 border-white" :
+          `absolute w-5 h-5 border-r-yellow-950 bottom-12 right-10 transition-opacity duration-1000 z-50 ease-in-out rounded-full bg-pink-500 shadow-lg cursor-pointer animate-bounce z-49 border-2 border-white ${arrowVisible ? "opacity-100" : "opacity-0"}`}
         onClick={handleLinkClick}
         ref={linkRef}
       >
-        <Image src={arrowDown} alt="Arrow down" className="w-auto h-auto rounded-full animate-bounce border-r-pink-500" />
+        <Image src={arrowDown} alt="Arrow down" className={isMobile ?
+          "w-auto h-auto rounded-full animate-bounce border-r-pink-500" :
+          "w-auto h-auto rounded-full animate-bounce border-r-pink-500"} />
       </Link>
 
       {/* Text Overlay */}
@@ -242,7 +250,7 @@ export default function Home() {
       {/* Hidden Video */}
       <video
         id="sourceVideo"
-        className="absolute opacity-0 object-cover"
+        className="absolute opacity-0"
         ref={videoRef}
         src="/myself.mp4"
         autoPlay
