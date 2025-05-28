88c03d71f8c8cdfe211312810753cf23c4479d72
mouveee <huwig.marco@gmail.com>
2025-05-28 14:04:51 +0200

    NavigationBar: Change symbol and placement

--- src/app/components/NavigationMenu.js
+++ src/app/components/NavigationMenu.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { AlignJustify } from 'lucide-react';
 
 const NavigationMenu = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -16,16 +17,16 @@ const NavigationMenu = () => {
   return (
     <div className="relative">
       <div
-        className="fixed h-10 w-10 flex items-center justify-center bottom-5 left-1/2 sm:left-auto sm:right-10 -translate-x-1/2 sm:bottom-10 sm:translate-x-0 p-4 rounded-full bg-pink-500 shadow-lg cursor-pointer animate-bounce z-50 border-2 border-white"
+        className="fixed h-10 w-10 flex items-center justify-center top-10 right-8 cursor-pointer z-50"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <div className=" text-2xl font-bold">!</div>
+        <AlignJustify className="w-20 h-20" color="#D8D1B1"/>
       </div>
 
       {/* Menu Container */}
       <nav
-        className={`fixed left-1/2 -translate-x-1/2 bottom-5 sm:left-auto sm:bottom-10 sm:translate-x-0 sm:right-10 p-4 bg-black  rounded-lg shadow-lg transition-transform transform duration-300 ease-in-out z-50 ${
+        className={`fixed top-10 right-8 p-4 bg-black  rounded-lg shadow-lg transition-transform transform duration-300 ease-in-out z-50 ${
           isHovered ? "scale-100 opacity-100" : "scale-0 opacity-0"
         }`}
         onMouseEnter={() => setIsHovered(true)}
