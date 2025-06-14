8cc1787aa1531d449848f1c8df7f2fd58f3fbe36
mouveee <huwig.marco@gmail.com>
2025-06-14 17:47:40 +0200

    auth: Add jwt stragedy

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -3,7 +3,11 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google({
-      clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     })],
+    session: {
+      strategy: 'jwt'
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 })
