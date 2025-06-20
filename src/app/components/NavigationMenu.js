3093f09496d38f0f7d8a8441b7185c377eecd77e
mouveee <huwig.marco@gmail.com>
2025-06-20 15:18:28 +0200

    NavigationMenu: Add overlay when hovered

--- src/app/components/NavigationMenu.js
+++ src/app/components/NavigationMenu.js
@@ -17,6 +17,11 @@ const NavigationMenu = () => {
   return (
     <div className="relative">
       <div
+        className={`overlay fixed w-[100vw] h-[100vh] bg-black ${isHovered ? 'bg-opacity-80' : 'bg-opacity-0'} z- top-0 left-0 transition-all duration-500`}
+      >
+      </div>
+
+      <div
         className="fixed h-20 w-20 flex items-start justify-end top-10 right-4 cursor-pointer z-50"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
